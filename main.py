c2f7a5578129c82738416b95ecb15a0ff0850b3e
FIRST_NAME LAST_NAME <wancerzk@gmail.com>
2023-07-16 19:48:17 +0200

    classless version of game

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import re
 board = [["*" for x in range(3)] for y in range(3)]
 regex_pattern = r'^[0-2]$'
+player = "X"
+
+
 
 def check_winner(player,board):
     if (( board[0][0] and board[0][1] and board[0][2] ) == player) or \
@@ -12,15 +15,26 @@ def check_winner(player,board):
         print("gracz wygrał:" + player)
         return True; 
     
-
-
-while 1:
-
+def print_board(board):
     for x in board:
         print("\n")
         for y in x:
-
             print(y+ " ", end="")
+
+
+def change_player(player):
+    if player == "X":
+        player = "O"
+    else:
+        player = "X"
+    print("Aktualny gracz:" + player)
+    return player
+
+while 1:
+    
+
+    print_board(board)
+
 
     try:
         x_player = input("podaj koordynat x (0-2):")
@@ -29,23 +43,26 @@ while 1:
 
         #Debug break
         if x_player == "Z":
-            break;
+            break
 
 
 
         if re.match(regex_pattern,x_player) and re.match(regex_pattern,y_player):
             y_player = int(y_player)
             x_player = int(x_player)
-            print(x_player)
-            print(y_player)
-            print(board[y_player][x_player])
-            print("pasuje")
-            # print(board[x_player][y_player])
-            if board[y_player][x_player] != "A":
-                board[y_player][x_player] = "A"
-                winner = check_winner("A",board)
+
+
+            # print(board[y_player][x_player])
+            if board[y_player][x_player]  != "O" and board[y_player][x_player]  != "X":
+                board[y_player][x_player] = player
+
+                winner = check_winner(player,board)
+
+
                 if winner == True:
-                    break;
+                    break
+                else:
+                    player = change_player(player)
             else:
                 print("pole już zajęte") 
  
